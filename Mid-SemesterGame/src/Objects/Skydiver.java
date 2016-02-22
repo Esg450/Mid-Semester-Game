05ae6a53649b09934407ba8c366badd556b0ced2
Graham <Graham@104.39.86.177>
2016-02-22 15:48:08 -0500

    Added some code to skydiver

--- Mid-SemesterGame/src/Objects/Skydiver.java
+++ Mid-SemesterGame/src/Objects/Skydiver.java
@@ -1,9 +1,42 @@
 package Objects;
 
+import java.awt.Image;
+import java.awt.Rectangle;
+
 /**
  *
  * @author evangentis
  */
-public class Skydiver {
+public class Skydiver extends Rectangle{
+    private int dx;
+    private int currentX;
+    private int y;
+    private Image skydiverImage;
+    
+    public Skydiver(int x){
+        currentX = x;
+        y = 0;
+        
+    }
+    
+    public int getCurrnetX(){
+        return this.currentX;
+    }
+    
+    public int getCurrnetY(){
+        return this.y;
+    }
+    
+    public void sexPosX(int changeInX){
+        this.dx = changeInX;
+        this.x = this.x + this.dx;
+    }
+    
+    public void sexPosY(int changeInY){
+        this.y = this.y + changeInY;
+    }
+    
+    
+    
     
 }
