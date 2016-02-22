e17e3bc35d50f2287249f601bbd4462caea4f08f
Esg450 <evan.gentis@gmail.com>
2016-02-22 15:44:41 -0500

    Added initial methods to obstacle class

--- Mid-SemesterGame/src/Objects/Obstacle.java
+++ Mid-SemesterGame/src/Objects/Obstacle.java
@@ -1,9 +1,44 @@
 package Objects;
 
+import java.awt.Graphics;
+import java.awt.Rectangle;
 /**
  *
  * @author evangentis
  */
-public class Obstacle {
+public class Obstacle extends Rectangle {
+    private int dy;
     
+    public Obstacle() {
+        dy = 0;
+    }
+    
+    public void setXPos(int i) {
+        this.x = i;
+    } 
+    
+    public void setYPos(int i) {
+        this.y = i;
+    }
+    
+    //Not yet implemented
+    public void getRandomImage() {
+        
+    }
+    
+    public int getCurrentX() {
+        return this.x;
+    }
+    
+    public int getCurrrentY() {
+        return this.y;
+    }
+    
+    public void drawImage(Graphics g) {
+        
+    }
+    
+    public void move() {
+        
+    }
 }
