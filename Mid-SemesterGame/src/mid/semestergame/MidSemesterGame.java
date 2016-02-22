94b565ceb99dc59c10757efb159385b2e18875e2
Esg450 <evan.gentis@gmail.com>
2016-02-22 15:09:33 -0500

    Initial Commit

---
+++ Mid-SemesterGame/src/mid/semestergame/MidSemesterGame.java
@@ -0,0 +1,16 @@
+package mid.semestergame;
+
+/**
+ *
+ * @author evangentis
+ */
+public class MidSemesterGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
