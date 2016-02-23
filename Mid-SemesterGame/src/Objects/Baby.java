e821ee5988f75332b909ba926565a4e093f7644f
Zack <Zack@104.38.88.135>
2016-02-23 17:13:36 -0500

    updated baby

--- Mid-SemesterGame/src/Objects/Baby.java
+++ Mid-SemesterGame/src/Objects/Baby.java
@@ -5,7 +5,11 @@
  */
 package Objects;
 
+import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -13,10 +17,12 @@ import java.awt.Rectangle;
  */
 public class Baby extends Rectangle{
     private int dy = 0;
+    private ImageIcon image = new ImageIcon();
    
     public Baby(int x, int y)
     {
         super.setLocation(x, y);
+        
     }
     public double getCurrentX()
     {
@@ -30,9 +36,22 @@ public class Baby extends Rectangle{
     {
         
     }
-    public void drawImage()
+    public void drawImage(Graphics g)
     {
-       
+        g.drawImage(image.getImage(), this.x, this.y, null);
+    }
+    public ImageIcon getRandomImage()
+    {
+        Random r = new Random();
+        int randNum = r.nextInt(3);
+        ArrayList<String> babyPics = new ArrayList<String>();
+        babyPics.add("babyPic1.jpg");
+        babyPics.add("babyPic2.jpg");
+        babyPics.add("babyPic3.jpg");
+        String randomImageFile = babyPics.get(randNum);
+        ImageIcon randomImage = new ImageIcon("src/images/"+randomImageFile);
+        return randomImage;
+        
     }
     
     
