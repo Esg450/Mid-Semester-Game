40366c07b51c1fc510371bb1fc383bd5a5a0e05a
matthewtucker <matthewtucker@client-104-39-86-65.mobility.psu.edu>
2016-02-29 17:01:00 -0500

    Added Instructions panel

---
+++ Mid-SemesterGame/src/mid/semestergame/InstructionsPanel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mid.semestergame;
+import javax.swing.*;
+/**
+ *
+ * @author matthewtucker
+ */
+public class InstructionsPanel extends JPanel {
+    private JLabel label1;
+    private JButton return1;
+    
+    InstructionsPanel(){
+        label1 = new JLabel ("Instructions");
+        add(label1);
+        return1 = new JButton("Return");
+        add(return1);
+    }
+}
